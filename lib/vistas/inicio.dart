2df51e6e92cb70e88ae2e39752d0bf6ea9d9ed01
Mauricio11222 <dennys.mejia@outlook.com>
2023-07-07 09:11:17 -0500

    catalogo

--- lib/vistas/inicio.dart
+++ lib/vistas/inicio.dart
@@ -1,15 +1,151 @@
+import 'dart:math';
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:learns/constantes.dart';
 
-class InicioPage extends StatelessWidget {
+class InicioPage extends StatefulWidget {
   const InicioPage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _InicioPageState createState() => _InicioPageState();
+}
+
+class _InicioPageState extends State<InicioPage> {
+  late PageController _pageController;
+  int _currentPage = 0;
+  final List<Widget> _pageItems = [];
+final int cantidadRecursos = 5; // Cantidad de recursos que deseas mostrar
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPage);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _buildPageItems();
+    super.didChangeDependencies();
+  }
+///// se crean los repositorios aleatorios 
+void _buildPageItems() {
+  const int itemsPerPage =4; // Cantidad de contenedores por página
+  int totalItems = 15; // Total de contenedores en tu lista
+  Random random = Random();
+
+  for (int i = 0; i < totalItems; i += itemsPerPage) {
+    List<Widget> page = [];
+
+    for (int j = i; j < i + itemsPerPage; j++) {
+      if (j < totalItems) {
+        Color randomColor = Color.fromARGB(
+          255,
+          random.nextInt(256),
+          random.nextInt(256),
+          random.nextInt(256),
+        );
+
+        page.add(
+          Expanded(
+            child: Container(
+              color: randomColor,
+                child: InkWell(
+                  onTap: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Image.asset(
+                          'assets/images/logo.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    
+                      Expanded(
+                        flex: 1,
+                        // ignore: avoid_unnecessary_containers
+                        child: Container(
+                          child: const AutoSizeText(
+                          'Nombre del recurso',
+                           style: TextStyle(color: Colors.white),
+                            minFontSize: 5.0, // Tamaño mínimo de la fuente
+                            maxFontSize: double.infinity, // Tamaño máximo de la fuente (se ajustará automáticamente)
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            ),
+                        ),
+                      ),
+                    ],
+                  ),
+              ),
+            ),
+          ),
+        );
+      }
+    }
+
+    _pageItems.add(
+      GridView.count(
+        crossAxisCount: itemsPerPage,
+        shrinkWrap: true,
+        children: page,
+      ),
+    );
+  }
+}
+
+
+
+  void _previousPage() {
+    if (_currentPage > 0) {
+      _pageController.previousPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+      setState(() {
+        _currentPage--;
+      });
+    } else {
+      _pageController.jumpToPage(_pageItems.length - 1);
+      setState(() {
+        _currentPage = _pageItems.length - 1;
+      });
+    }
+  }
+
+  void _nextPage() {
+    if (_currentPage < _pageItems.length - 1) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+      setState(() {
+        _currentPage++;
+      });
+    } else {
+      _pageController.jumpToPage(0);
+      setState(() {
+        _currentPage = 0;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: color1,
-        leading:
+         leading:
             Flexible(flex: 1, child: Image.asset("assets/images/logo.png")),
         title: const Text('    L.E.A.R.N.S   ESPOCH'),
         actions: [
@@ -67,6 +203,13 @@ class InicioPage extends StatelessWidget {
                         // Lógica para manejar la selección de "Informática y electrónica"
                       },
                       child: const Text('Informática y electrónica'),
+                    ),
+                   PopupMenuItem(
+                      value: 2,
+                      onTap: () {
+                        // Lógica para manejar la selección de "Informática y electrónica"
+                      },
+                      child: const Text('Mecánica'),
                     ),
                     // Agrega más opciones de menú según tus necesidades
                   ],
@@ -128,138 +271,27 @@ class InicioPage extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 70),
-      LayoutBuilder(
-  builder: (BuildContext context, BoxConstraints constraints) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          Container(
-            width: constraints.maxWidth * 0.2, // Ajusta el tamaño del contenedor según tus necesidades
-            height: 200,
-            color: Colors.blue,
-            child: InkWell(
-              onTap: () {
-                // Acción al presionar la imagen
-                // Puedes ejecutar una función o navegar a otra pantalla, por ejemplo
+
+          ////aqui se ponen los repositorios ahora esta solo creandose aleatoriamente pero deberia ir con una base de datos
+          SizedBox(
+            height: 250,
+            child: PageView(
+              controller: _pageController,
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (int page) {
+                setState(() {
+                  _currentPage = page;
+                });
               },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/logo.png'),
-                  const Text(
-                    'Nombre del recurso',
-                    style: TextStyle(fontSize: 10.0, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(width: 20),
-          Container(
-            width: constraints.maxWidth * 0.2, // Ajusta el tamaño del contenedor según tus necesidades
-            height: 200,
-            color: const Color.fromARGB(255, 33, 243, 107),
-            child: InkWell(
-              onTap: () {
-                // Acción al presionar la imagen
-                // Puedes ejecutar una función o navegar a otra pantalla, por ejemplo
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/logo.png'),
-                  const Text(
-                    'Nombre del recurso',
-                    style: TextStyle(fontSize: 10.0, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(width: 20),
-          Container(
-           width: constraints.maxWidth * 0.2, // Ajusta el tamaño del contenedor según tus necesidades
-            height: 200,
-            color: const Color.fromARGB(255, 243, 33, 33),
-            child: InkWell(
-              onTap: () {
-                // Acción al presionar la imagen
-                // Puedes ejecutar una función o navegar a otra pantalla, por ejemplo
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/logo.png'),
-                  const Text(
-                    'Nombre del recurso',
-                    style: TextStyle(fontSize: 10.0, color: Colors.white),
-                  ),
-                ],
-              ),
+              children: _pageItems,
             ),
           ),
 
-           const SizedBox(width: 20),
-          Container(
-           width: constraints.maxWidth * 0.2, // Ajusta el tamaño del contenedor según tus necesidades
-            height: 200,
-            color: Color.fromARGB(255, 182, 223, 18),
-            child: InkWell(
-              onTap: () {
-                // Acción al presionar la imagen
-                // Puedes ejecutar una función o navegar a otra pantalla, por ejemplo
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/logo.png'),
-                  const Text(
-                    'Nombre del recurso',
-                    style: TextStyle(fontSize: 10.0, color: Colors.white),
-                  ),
-
-                  
-                ],
-              ),
-            ),
-          ),
-
-          const SizedBox(width: 20),
-          Container(
-           width: constraints.maxWidth * 0.2, // Ajusta el tamaño del contenedor según tus necesidades
-            height: 200,
-            color: Color.fromARGB(255, 18, 223, 216),
-            child: InkWell(
-              onTap: () {
-                // Acción al presionar la imagen
-                // Puedes ejecutar una función o navegar a otra pantalla, por ejemplo
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/logo.png'),
-                  const Text(
-                    'Nombre del recurso',
-                    style: TextStyle(fontSize: 10.0, color: Colors.white),
-                  ),
-
-                  
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  },
-),
-
-
+          
         ],
       ),
       bottomNavigationBar: Container(
-        height: 100.0,
+        height: 110.0,
         color: Colors.grey,
         child: const Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -271,7 +303,6 @@ class InicioPage extends StatelessWidget {
                 style: TextStyle(fontSize: 14.0, color: Colors.white),
               ),
             ),
-            // Espacio horizontal entre los widgets
             Text(
               '    Dirección: Panamericana sur km 1 1/2. Riobamba-Ecuador',
               style: TextStyle(fontSize: 12.0, color: Colors.white),
@@ -287,9 +318,25 @@ class InicioPage extends StatelessWidget {
             Text(
               '    Código Postal: EC060155',
               style: TextStyle(fontSize: 12.0, color: Colors.white),
-            )
+            ),
           ],
         ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Padding(
+      padding: const EdgeInsets.only(left: 10.0), // Ajusta el espaciado aquí
+      child: IconButton(
+        onPressed: _previousPage,
+        icon: const Icon(Icons.arrow_back),
+      ),
+    ),
+    IconButton(
+      onPressed: _nextPage,
+      icon: const Icon(Icons.arrow_forward),
+    ),
+        ],
       ),
     );
   }
